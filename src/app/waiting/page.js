3ad933b986019d6649ef754a8e1e433d20b4ae9f
ihababbas <ihababbas26@gmail.com>
2023-07-30 10:54:08 +0300

    add pages

--- src/app/waiting/page.js
+++ src/app/waiting/page.js
@@ -22,39 +22,58 @@ const WaitingPage = () => {
 
   return (
     <>
-    <div className="container mx-auto h-screen flex justify-center items-center">
-      {/* First Column */}
-      <div className="w-60% mr-4">
-        <div className="mb-6">
-          <div className="border-b-2 px-2 py-3 text-lg font-bold">Row 1</div>
-        </div>
-        <div className="mb-6">
-          <div className="border-b-2 px-2 py-3 text-lg font-bold">Row 2</div>
-        </div>
-        <div className="flex">
-          <div className="w-1/2 border-r-2">
-            <div className="border-b-2 px-2 py-3 text-lg font-bold">Row 3 (Part 1)</div>
-          </div>
-          <div className="w-1/2">
-            <div className="border-b-2 px-2 py-3 text-lg font-bold">Row 3 (Part 2)</div>
+      
+      {isLoading ? (
+        <div className="container mx-auto h-screen flex justify-center items-center">
+          <div className="text-center">
+            <div className="animate-spin text-3xl mb-4">&#128259;</div>
+            <p>Loading...</p>
           </div>
         </div>
-        <div className="flex">
-          <div className="w-1/2 border-r-2">
-            <div className="border-b-2 px-2 py-3 text-lg font-bold">Row 4 (Part 1)</div>
-          </div>
-          <div className="w-1/2">
-            <div className="border-b-2 px-2 py-3 text-lg font-bold">Row 4 (Part 2)</div>
-          </div>
-        </div>
+      ) : (
+       
+        <div >
+      
+        <div className="flex h-screen">
+      {/* First Section (Gap) */}
+      <div className="flex-1"></div>
+
+      {/* Second Section (Table) */}
+      <div className="flex-[2] flex items-center justify-center">
+      <div className="flex justify-center items-center h-screen">
+      <table className="border-collapse border border-solid border-[black]">
+        <tbody>
+          <tr>
+            <td colSpan="2" className='border px-5 py-2.5 border-solid border-[black]'>Row 1</td>
+          </tr>
+          <tr>
+            <td colSpan="2" className='border px-5 py-2.5 border-solid border-[black]'>Row 2</td>
+          </tr>
+          <tr>
+            <td className='border px-5 py-2.5 border-solid border-[black]'>Row 3 - Part 1</td>
+            <td className='border px-5 py-2.5 border-solid border-[black]'>Row 3 - Part 2</td>
+          </tr>
+          <tr>
+            <td className='border px-5 py-2.5 border-solid border-[black]'>Row 4 - Part 1</td>
+            <td className='border px-5 py-2.5 border-solid border-[black]'>Row 4 - Part 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
       </div>
 
-      {/* Second Column */}
-      <div className="w-40%">
-        <div className="w-32 h-32 rounded-full bg-gray-300 mb-6"></div>
-        <div className="text-center font-bold text-xl">50%</div>
+      {/* Third Section (Circle and Mark %) */}
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <div className="w-[100px] h-[100px] bg-[#007bff] rounded-[50%]"></div>
+        <div className="text-2xl text-[#007bff]">%</div>
       </div>
+
+      {/* Fourth Section (Gap) */}
+      <div className="flex-1"></div>
     </div>
+    </div>
+      )}
+
     <Footer/>
     </>
   );
